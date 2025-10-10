17ec2b4fc73981bf04c0cfa98eb4fdf8c822c421
ruijiuchen <r.chen@gsi.de>
2025-10-10 10:49:34 +0800

    modify requirements.txt

--- rionidgui/__main__.py
+++ rionidgui/__main__.py
@@ -3,10 +3,12 @@ from PyQt5.QtWidgets import QApplication
 from rionidgui.gui import MainWindow
 
 def main():
+    print("test...")
     app = QApplication(sys.argv)
+    print("test2 ...")
     main_window = MainWindow()
     main_window.show()
-    sys.exit(app.exec_())
+    #sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()

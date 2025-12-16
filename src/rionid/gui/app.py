6aee852c2e3896ec3132101c153a807c876801e1
DFreireF <D.FreireFernandez@gsi.de>
2025-12-16 11:18:21 +0100

    Moved main contents to app.py for simplicity mar 16 dic 2025 11:18:21 CET

--- src/rionid/gui/app.py
+++ src/rionid/gui/app.py
@@ -50,3 +50,13 @@ class MainWindow(QWidget):
     def update_visualization(self, data):
         """This method updates the visualization panel with new data."""
         self.visualization_widget.updateData(data)
+
+def main():
+    """Entry point for the RionID application."""
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
